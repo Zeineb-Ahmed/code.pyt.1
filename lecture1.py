096d7dfdb7ae341f2e5d42707d2d74d79ddef73a
zeineb ahmed <benhamed.zeineb@hotmail.com>
2024-01-03 22:29:52 +0100

    adding files

---
+++ lecture1.py
@@ -0,0 +1,96 @@
+""" 
+*What is Python?
+# 00 11 0 1 01001
+001001100111
+write
+*What are the characteristics of high-level programming languages compared to low-level programming languages?
+*Who is the inventor of Python? Guido van  rossum Deutsh
+*When was Python invented? 1991
+*Why should one learn Python, and in what applications can Python be used?
+-readability
+-versability
+-community
+-job opportunities
+*Is Python considered a scripting language or a programming language?
+*What is the difference between coding and programming?
+*What is the latest version of Python? v3
+*What do you need to install before using Python? 
+"""
+#output print
+#input 
+#data types
+#operators
+#casting
+#Practice
+#math functions
+# Basic Operations
+""" math.add(x, y)
+math.subtract(x, y)
+math.multiply(x, y)
+math.divide(x, y)
+math.pow(base, exponent)
+math.sqrt(x)
+math.abs(x)
+math.round(x, ndigits)
+math.factorial(n)
+math.trunc(x)
+ math.ceil(x)
+ math.floor(x) """
+#output
+#variables
+""" v="121"
+print(v) """
+#data types
+# nom de produit     prix-produit     annee     quantite    disponibilite
+#   jus                  2.500         2023       10            vrai/True
+#   biscuit               3.200         2018      30            faux/false
+#   String/Str " "       float          int       int           boolean bool
+""" nom="jus"  #  "any value"
+prix=2.500  # float avec virgule
+annee=2023  # int sans virgule
+quantite=10  # int sans virgule
+disponibilite=True # bool True
+ """
+""" print(type(2000.5))
+# print("bonsoir") """
+# single line comment
+"""
+print("bonsoir")
+print("bonjour")
+print("hello")
+"""
+""" nom=input()
+print("bonjour "+nom) """
+
+""" vStr=input()# "True"
+vInt=bool(vStr) # "90" --> 90
+print(type(vStr)) """
+""" 
+valeur1=input() 
+valeur2=input()  
+somme=int(valeur1)+int(valeur2)
+print(somme) """ 
+#operators
+v1=375
+v1/=50 #v1=v1/50
+v1=v1+50
+# 2a la puissance 5 = 2*2*2*2*2
+print(v1)
+"""
++ addition
+-  soustraction
+*  multiplication
+/  division
+%  reste de la division
+** puissance
+= affectation
+== egalite'
+!=different
++=  # v1+=50    --> v1=v1+50
+-=  # v1-=50    --> v1=v1-50
+*=  # v1=v1*50
+/=  #v1=v1/50
+"""
+
+
+
